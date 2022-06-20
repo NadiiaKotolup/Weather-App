319b43be0161f363463cc04a2bf174a0fa5ebb23
NadiiaKotolup <nkotolup@gmail.com>
2022-06-20 11:08:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "62ac36dc9562f6ebc71a8175884314a0";
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
